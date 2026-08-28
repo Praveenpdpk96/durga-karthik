81d7aafe40fb74c7dc8e3c0963bc258f05a91cb2
Praveenpdpk96 <praveenpdpk96@gmail.com>
2026-08-27 22:26:37 -0500

    Add Spring Boot application entry point

---
+++ src/main/java/com/praveen/career/CareerIntelligenceApplication.java
@@ -0,0 +1,11 @@
+package com.praveen.career;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerIntelligenceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CareerIntelligenceApplication.class, args);
+    }
+}
