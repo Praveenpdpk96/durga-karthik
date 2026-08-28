c97f15c23fe6cd67dea7fd577c04f79b15aa1026
Praveenpdpk96 <praveenpdpk96@gmail.com>
2026-08-27 22:26:56 -0500

    Expose resume job matching API

---
+++ src/main/java/com/praveen/career/matching/MatchingController.java
@@ -0,0 +1,24 @@
+package com.praveen.career.matching;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/matches")
+public class MatchingController {
+
+    private final MatchingService matchingService;
+
+    public MatchingController(MatchingService matchingService) {
+        this.matchingService = matchingService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MatchResponse> match(@Valid @RequestBody MatchRequest request) {
+        return ResponseEntity.ok(matchingService.match(request));
+    }
+}
